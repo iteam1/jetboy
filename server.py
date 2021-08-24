8928c46fdc5fa1f1fb74dfefb4d88f830b418c1e
chuongloc <chuongloc@vasbit.com>
2021-08-24 16:13:10 +0700

    chuongloc
    add new page

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ mrcnn = MaskRCNN()
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+	return render_template('page_home.html')
+
 @app.route('/view')
 def normal_view():
 	return render_template('page_view.html')
