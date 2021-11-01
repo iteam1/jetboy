75a330b10eac216e7d39a3ab25e8929b2d10416a
LocChuong <locchuong123@gmail.com>
2021-11-01 10:31:51 +0700

    LocChuong

--- control_servo.py
+++
@@ -1,8 +0,0 @@
-# control PCA9685 board
-# pip3 install adafruit-circuitpython-servokit
-import time 
-from adafruit_servokit import ServoKit
-
-kit = ServoKit(channels = 16)
-
-kit.servo[0].angle = 90
