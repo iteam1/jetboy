c57e2d6cfa60d25d1a10bb820ca71bddde5c0bab
LocChuong <locchuong123@gmail.com>
2021-12-22 10:07:24 +0700

    Add database_reader.py
    locchuong

---
+++ robot/database_reader.py
@@ -0,0 +1,38 @@
+'''
+This python program continously read all value of the database print it out the screen
+'''
+import sqlite3
+import time  
+
+id = int(input("What is your robot's id? "))
+
+conn = sqlite3.connect('./site.db') # Create the connection to the database
+
+c = conn.cursor() # Create the cursor for the connection
+
+column_list = ['id','name','command','content','emotion','image','itype','estop','obs_f','obs_b','obs_l','obs_r']
+
+print("Start reading robot id = {id}")
+
+while True:
+	c.execute(f"""
+		SELECT *FROM robot WHERE id = {id} """)
+	conn.commit()
+
+	data = c.fetchone()
+
+	message = "("
+	for i in range(len(data)):
+		if i == len(data) - 1: 
+			message = message + column_list[i] + ' = ' + str(data[i])
+		else:
+			message = message + column_list[i] + ' = ' + str(data[i]) + ','
+
+	message = message + ")" 
+
+	print(message)
+
+	time.sleep(0.01)
+
+conn.close()
+
