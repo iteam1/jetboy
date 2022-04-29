1bc48a2407cc46eb36a2facc754b9c54f16d456b
locchuong <locchuong123@gmail.com>
2022-04-29 16:41:32 +0700

    edit robot arm code
    locchuong

--- snippet/comm_serial.py
+++ snippet/comm_serial.py
@@ -12,5 +12,7 @@ arduino = serial.Serial(port,baudrate = 9600,timeout = 0.1)
 if __name__ == "__main__":
 	while True:
 		x = input('Enter the framework: ')
+		x = x+ "a"
+		print(x)
 		#arduino.write(bytes(x,'utf-8'))
 		arduino.write(x.encode())
