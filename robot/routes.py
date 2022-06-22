4308d8474d1029567d9021d1587b391a896f08bb
locchuong <locchuong123@gmail.com>
2022-06-22 16:08:19 +0700

    add kill gpio controller path

--- robot/routes.py
+++ robot/routes.py
@@ -1,6 +1,6 @@
 '''
 Author: locchuong
-Updated: 27/12/21
+Updated: 22/6/22
 Description: 
 	This is contain routes of the flask server
 '''
@@ -213,9 +213,11 @@ def depthstream():
 def cvstream():
 	return Response(gen_cvframe(),mimetype = 'multipart/x-mixed-replace; boundary=frame')
 
-
 @app.route('/content',methods =['POST'])
 def content():
+	'''
+	change display screen to display a content for robot_gui 
+	'''
 	#if request.method == 'POST':
 	content = request.form.get('content')
 	myrobot = Robot.query.get(1)
@@ -227,6 +229,9 @@ def content():
 	
 @app.route('/emotion',methods =['POST'])
 def emotion():
+	'''
+	change display screen to emotion for robot_gui 
+	'''
 	emotion = request.form.get('emotion')
 	myrobot = Robot.query.get(1)
 	myrobot.itype = 'emo'
@@ -235,8 +240,11 @@ def emotion():
 	db.session.commit()
 	return render_template('manual.html',estop = estop)
 	
-@app.route('/image',methods =['GET','POST'])
+@app.route('/image',methods =['POST'])
 def image():
+	'''
+	change display screen to image for robot_gui 
+	'''
 	image = request.form.get('image')
 	myrobot = Robot.query.get(1)
 	myrobot.itype = 'img'
