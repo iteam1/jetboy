8720bd55a8553692c02719061315974aff9863de
locchuong <locchuong123@gmail.com>
2022-06-22 10:42:45 +0700

    rebuild database reader function

--- utils/database_reader.py
+++ utils/database_reader.py
@@ -1,47 +1,58 @@
 '''
 Author: locchuong
-Updated: 27/12/21
+Updated: 22/6/21
 Description:
 	This python program continously read all value of the database print it out the screen
 	Run this python program on the directory ./Jetson-Nano
 '''
 
-import sqlite3
-import time 
 import os 
+import time
+import sqlite3
+import argparse
+from datetime import datetime
 
-id = int(input("What is your robot's id? "))
+# init parser
+parser = argparse.ArgumentParser(description = ' Query database and print it out continously')
+# add argument to parser
+parser.add_argument('-n','--id', type = int, help = 'robot id', required = True)
+parser.add_argument('-t','--time',type = float,help = 'timedelay display', default = 0.1)
+# create arguments
+args = parser.parse_args()
 
+# connect to database sqlite3
 conn = sqlite3.connect('./robot/site.db') # Create the connection to the database
-
 c = conn.cursor() # Create the cursor for the connection
-
+print('database connected!')
+# check database id
+# id = int(input("What is your robot's id? "))
+# what column that you want to read
 column_list = ['id','name','command','content','emotion','image','itype','estop','obs_f','obs_b','obs_l','obs_r']
-
 print("Start reading robot id = {id}")
 
-while True:
-	
-	c.execute(f"""
-		SELECT *FROM robot WHERE id = {id} 
-		""")
-	
-	conn.commit()
+if __name__ == "__main__":
+	while True:
+		# query database
+		c.execute(f"""
+			SELECT *FROM robot WHERE id = {args.id} 
+			""")
+		conn.commit()
+		# get data follow your query
+		data = c.fetchone() # a tuple
+		# make the message
+		message = str(datetime.now()) + "\n{\n" 
+		for i in range(len(data)):
+			# if you to the last of message then don't have to add comma
+			if i == len(data) - 1: 
+				message = message + column_list[i] + ' = ' + str(data[i])
+			else:
+				message = message + column_list[i] + ' = ' + str(data[i]) + ', \n'
 
-	data = c.fetchone()
+		message = message + "\n}\n" 
 
-	message = "("
-	for i in range(len(data)):
-		if i == len(data) - 1: 
-			message = message + column_list[i] + ' = ' + str(data[i])
-		else:
-			message = message + column_list[i] + ' = ' + str(data[i]) + ','
+		print(message)
+		time.sleep(0.1)
+		os.system('clear')
 
-	message = message + ")" 
-
-	print(message)
-
-	time.sleep(0.01)
-
-conn.close()
+	conn.close()
 
