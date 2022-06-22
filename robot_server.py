09eea1b24a996963ec4f23cbb645584210e55594
locchuong <locchuong123@gmail.com>
2022-06-22 16:43:53 +0700

    building shutdown function

--- robot_server.py
+++ robot_server.py
@@ -1,11 +1,19 @@
 '''
 Author: locchuong
-Updated: 27/12/21
+Updated: 22/6/22
 Description:
 	Run the server.py to create a local to interface with the robot
 '''
 # This is the main server for control robot
-from robot import app 
+from robot import app
+from robot import d455
 
 if __name__ == "__main__":
-	app.run(host = '0.0.0.0', port = 5000)
+
+	app.run(host = '0.0.0.0', port = 5000) #loop over
+
+	print('Server shutdowned!')
+	
+	d455.release()
+
+	print('Camera released!')
