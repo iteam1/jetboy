97ca7cdbbd6664aecf8cedcfd614bc60292fe220
locchuong <locchuong123@gmail.com>
2022-07-01 14:48:36 +0700

    display pointcloud

--- utils/show_pointcloud.py
+++ utils/show_pointcloud.py
@@ -9,14 +9,14 @@ import numpy as np
 import argparse  
 
 parser = argparse.ArgumentParser(description = "Show pointcloud .ply file")
-parser.add_argument('-d','--directory',type = str,help='the directory to pointcloud',required = True)
+parser.add_argument('-f','--filename',type = str,help='the name of pointcloud',required = True)
 args = parser.parse_args()
 
 if __name__ == "__main__":
 
 	# show point cloud
 	print("Load a ply point cloud cloud, print it and render it")
-	pcd = o3d.io.read_point_cloud(args.directory)
+	pcd = o3d.io.read_point_cloud("./pointcloud/" + args.filename + ".ply")
 	print(pcd)
 	print(np.asarray(pcd.points))
 	o3d.visualization.draw_geometries([pcd])
