9a0e5eadc812df56415b2f737c868c1f675cba6b
locchuong <locchuong123@gmail.com>
2022-08-05 14:53:47 +0700

    build test stepper motor method

--- embed/Stepper/Stepper.ino
+++ embed/Stepper/Stepper.ino
@@ -1,5 +1,12 @@
 # include <Servo.h> // Servo lib
-
+# include <SoftwareSerial.h> // create a new serial port
+# include <avr/io.h>
+# include <avr/interrupt.h>
+byte reload = 50; //set 100us period for interrupt timer
+int count = 0; //make a pulse for step motor if count = 8
+unsigned long T_delay; // T_delay for delay function
+int STmoi = 0; // the value of angle calculated by set pulse - current
+int STcu = 0;
 Servo sv1; // create rc servo object for join1
 
 void setup() {
@@ -9,6 +16,6 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sv1.write(180); // raise the arm before rotate step motor
+  sv1.write(50); // raise the arm before rotate step motor
 
 }
