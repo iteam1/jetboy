07f3e9197a680ac8ff87010b9751e54fd5e5b613
locchuong <locchuong123@gmail.com>
2022-08-05 16:23:18 +0700

    add test stepper motor

--- embed/Stepper/Stepper.ino
+++ embed/Stepper/Stepper.ino
@@ -1,10 +1,11 @@
 # include <Servo.h> // Servo lib
-Servo sv1; // create rc servo object for join1
+Servo sv1,sv2,sv3,sv4;  // robot's arm have 4 rc servo
 # include <SoftwareSerial.h> // create a new serial port
-# include <avr/io.h>
-# include <avr/interrupt.h>
-byte reload = 50; //set 100us period for interrupt timer
-int count = 0; //make a pulse for step motor if count = 8
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <EEPROM.h>
+byte reload = 50;  //100us for interupt timer
+int count = 0; //make a pulse for step motor if count == 8
 unsigned long T_delay; // T_delay for delay function
 int STmoi = 0; // the value of angle calculated by set pulse - current
 int STcu = 0; // the old value angle of step motor for comparing with new current value angle
@@ -18,8 +19,11 @@ void setup() {
   // init serial communication
   Serial.begin(9600); // Arduino internal serial
   Serial.println("step1: init serial -> DONE");
-  // init servo
-  sv1.attach(3);
+  // init RC servos
+  sv1.attach(9);  // attaches the servo on pin 9 to the servo object
+  sv2.attach(6);
+  sv3.attach(5);
+  sv4.attach(3);
   Serial.println("step2: init RC servo -> DONE");
   // init pin mode
   pinMode(stepPin,OUTPUT);
@@ -29,22 +33,63 @@ void setup() {
   pinMode(LED,OUTPUT);
   Serial.println("step3: init pins -> DONE");
   // init timer
-  cli(); // disable all interrupt before you done setting up
   Serial.println("step4: init timer -> DOING...");
-  //beep(5);
+//  cli(); // disable all interrupt before you done setting up
+//  TCCR0B = 0; 
+//  OCR2A = reload;
+//  TCCR2A = 1<<WGM21;
+//  TCCR2B = ((1<<CS21) | (1<<CS20));//1<<CS22)
+//  TIMSK2 = (1<<OCIE2A);
+//  sei();
+  // init stepper motor
+  digitalWrite(enPin,0); //0 la BRAKE, 1 la REALSE
+  digitalWrite(dirPin,0);
+  digitalWrite(stepPin,0);
+  // sound out
+  beep(5,100);
+  stand_by();
+  rotate();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  sv1.write(90); // raise the arm before rotate step motor
-
+  
 }
 
-void beep(int times){
+void rotate(){
+  digitalWrite(enPin,1); //0 la BRAKE, 1 la REALSE
+  digitalWrite(dirPin,0);
+  digitalWrite(stepPin,0);
+  
+  for(int i = 0; i <= 300; i++){
+    digitalWrite(stepPin,1);
+    delayMicroseconds(500);
+    digitalWrite(stepPin,1);
+    delayMicroseconds(500);
+    }
+  
+  digitalWrite(dirPin,1);
+  
+  for(int i = 0; i <= 300; i++){
+    digitalWrite(stepPin,1);
+    delayMicroseconds(500);
+    digitalWrite(stepPin,1);
+    delayMicroseconds(500);
+    }
+  }
+
+void stand_by(){
+  sv1.write(50); // raise the arm before rotate step motor
+  sv2.write(90);
+  sv3.write(0);
+  sv4.write(170); 
+  }
+
+void beep(int times,int sleep){
   for(int i = 0;i<=times;i++){
     digitalWrite(BZ,1);
-    delay(1000);
+    delay(sleep);
     digitalWrite(BZ,0);
-    delay(1000);
+    delay(sleep);
     }
   }
---
+++ embed/Stepper2/Stepper2.ino
@@ -0,0 +1,70 @@
+/*
+ * https://nshopvn.com/product/mach-dieu-khien-dong-co-buoc-tb6600-5a-hy-div268n/?gclid=Cj0KCQjw_7KXBhCoARIsAPdPTfi90oOPmzix3mB6K9zfVeSNNHptESnVxldMXMKFD_zgTHs-L6sKvdsaAnjvEALw_wcB
+  Kết nối:
+          TB6600                  Arduino
+           ENA+                      8
+           DIR+                      2
+           PUL+                      5 
+           ENA-                     GND
+           DIR-                     GND
+           PUL-                     GND
+
+  A+ A- B+ B- kết nối với động cơ
+
+  Nguồn đầu vào là 9V - 42V.
+ */
+# include <Servo.h> // Servo lib
+Servo sv1,sv2,sv3,sv4;  // robot's arm have 4 rc servo
+
+const int stepPin = 4; // make pulse for step motor
+const int dirPin = 7; // direction for step motor pin
+const int enPin = 8; //brake step motor pin
+void setup() {
+
+  // init servo
+  sv1.attach(9);  // attaches the servo on pin 9 to the servo object
+  sv2.attach(6);
+  sv3.attach(5);
+  sv4.attach(3);
+  
+  // Sets the two pins as Outputs
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+
+  pinMode(enPin,OUTPUT);
+  digitalWrite(enPin,LOW);
+
+  // stand by postion
+  stand_by();
+  
+}
+void loop() {
+  
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 800; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+  delay(1000); // One second delay
+
+  digitalWrite(dirPin,LOW); //Changes the rotations direction
+  // Makes 400 pulses for making two full cycle rotation
+  for(int x = 0; x < 800; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+  
+}
+
+void stand_by(){
+  sv1.write(50); // raise the arm before rotate step motor
+  sv2.write(90);
+  sv3.write(0);
+  sv4.write(170); 
+  }
