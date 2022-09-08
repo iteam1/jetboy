0fd8d7026fc1f554731ee95a54f17ecb703feb7e
locchuong <locchuong123@gmail.com>
2022-09-08 15:05:50 +0700

    building face recognition

---
+++ utils/face_rec.py
@@ -0,0 +1,38 @@
+'''
+Author: locchuong
+Updated: 8/9/22
+Description:
+    Test face recognition realtime
+'''
+
+import cv2
+import argparse
+import face_recognition
+
+# init parser
+parser  = argparse.ArgumentParser(description = 'test stream camera')
+# add argument to parser
+parser.add_argument('-i','--id',type = int,required = True,help = 'camera number', default = 0)
+# create arguments
+args = parser.parse_args()
+
+if __name__ == "__main__":
+
+    cap = cv2.VideoCapture(args.id,cv2.CAP_DSHOW) # On windows cv2.CAP_DSHOW
+
+    ret,frame = cap.read()
+
+    while ret:
+        
+        ret,frame = cap.read()
+        
+        cv2.imshow('frame',frame)
+        
+        if cv2.waitKey(1) == 27:
+            break 
+        
+    cv2.destroyAllWindows()
+    cap.release()
+    print('exiting...')
+    exit()
+
