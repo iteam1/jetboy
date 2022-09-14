ae48e7e0d1a14d97ae45fbdcbe976d5cc280d4aa
locchuong <locchuong123@gmail.com>
2022-09-14 13:55:05 +0700

    building face recognition mission

---
+++ greeting.py
@@ -0,0 +1,12 @@
+'''
+Author: locchuong
+Updated: 14/9/22
+Description:
+	This is the first mission of robot intergate A.I,
+'''
+
+def main:
+	print("Hello World!")
+
+if __main__ == "__main__":
+	main()
